43485c0c968a53d9dae9ac2d3c50fd2b1d80ad93
YangEzzz <yez0807@163.com>
2023-05-21 15:04:54 +0800

    流式传输尝试

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.GPT_KEY || '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',
 });
-const sse = require('sse-node')
+const webSocket = require('ws')
+
+const ws = new WebSocket.Server({port: 3000})
 const openai = new OpenAIApi(configuration)
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -31,47 +33,55 @@ router.post('/chat', async (ctx, next) => {
   }
 })
 
-router.post('/sseChat', async (ctx)=>{
-  const { question } = ctx.request.body
-  ctx.status = 200
-  ctx.req.socket.setTimeout(10000)
-
-  const stream = sse(ctx.req, ctx.res)
-
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: question.messages,
-    stream: true,
-  }, { responseType: 'stream' });
-
-
-
-  const gptStream = response.data
-
-  gptStream.on('data', (chunk) => {
-    // Messages in the event stream are separated by a pair of newline characters.
-    const payloads = chunk.toString().split("\n\n")
-    for (const payload of payloads) {
-      if (payload.includes('[DONE]')) return;
-      if (payload.startsWith("data:")) {
-        const data = payload.replaceAll(/(\n)?^data:\s*/g, ''); // in case there's multiline data event
-        try {
-          const delta = JSON.parse(data.trim())
-          console.log(delta.choices[0].delta?.content)
-          stream.sendEvent({
-            data: delta.choices[0].delta?.content,
-            event: 'message',
-            id: new Date().toISOString(),
-          });
-        } catch (error) {
-          console.log(`Error with JSON.parse and ${payload}.\n${error}`)
-        }
-      }
-    }
+ws.on('connection', (ws)=> {
+  console.log('ws open')
+  ws.on('message', (message) => {
+    console.log('received', message)
+    ws.send('test reponse')
   })
-  gptStream.on('end', () => console.log('Stream done'))
-  gptStream.on('error', (e) => console.error(e))
 })
+
+// router.all('/sseChat', async (ctx)=>{
+//   const { question } = ctx.request.body
+//   ctx.status = 200
+//   ctx.req.socket.setTimeout(10000)
+//
+//   const stream = sse(ctx.req, ctx.res)
+//
+//   const response = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: question.messages,
+//     stream: true,
+//   }, { responseType: 'stream' });
+//
+//
+//
+//   const gptStream = response.data
+//
+//   gptStream.on('data', (chunk) => {
+//     // Messages in the event stream are separated by a pair of newline characters.
+//     const payloads = chunk.toString().split("\n\n")
+//     for (const payload of payloads) {
+//       if (payload.includes('[DONE]')) return;
+//       if (payload.startsWith("data:")) {
+//         const data = payload.replaceAll(/(\n)?^data:\s*/g, ''); // in case there's multiline data event
+//         try {
+//           const delta = JSON.parse(data.trim())
+//           console.log(delta.choices[0].delta?.content)
+//           stream.sendEvent({
+//             data: delta.choices[0].delta?.content,
+//             event: 'message',
+//             id: new Date().toISOString(),
+//           });
+//         } catch (error) {
+//           console.log(`Error with JSON.parse and ${payload}.\n${error}`)
+//         }
+//       }
+//     }
+//   })
+//   gptStream.on('end', () => console.log('Stream done'))
+//   gptStream.on('error', (e) => console.error(e))
+// })
 
 // router.post('/chatStream', async (ctx, next) => {
 //   const { question } = ctx.request.body
