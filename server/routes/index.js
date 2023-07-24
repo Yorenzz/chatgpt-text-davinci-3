9dee6ae5eca83a79557873a9f1c170ffcbb0d149
yorenz <yorenz_yang@ogcloud.com>
2023-07-24 12:17:04 +0800

    blob尝试

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,10 +23,10 @@ router.get('/string', async (ctx, next) => {
 router.post('/recordTranslate', async (ctx, next) => {
   // console.log(file)
   const fileObj = ctx.request.files.file // 获取上传文件
-  console.log(typeof fileObj, fileObj)
+  console.log(typeof fileObj)
   // file包含了文件名，文件类型，大小，路径等信息
   const res = await openai.createTranslation(
-    '/blob'
+    fileObj, "whisper-1"
   )
   console.log(res)
   ctx.body = res
